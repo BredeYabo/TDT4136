da6f475f6eff33984c621fec0815cc95e6440a63
bkr001 <bkr@ambita.com>
2017-11-10 13:17:58 +0100

    Removed Non-ASCII characters

--- assignment5.py
+++ assignment5.py
@@ -9,12 +9,12 @@ function BACKTRACKING-SEARCH(csp) returns a solution, or failure
 
 function BACKTRACK(assignment,csp) returns a solution, or failure
     if assignment is complete then return assignment
-    var ← SELECT-UNASSIGNED-VARIABLE(csp)
+    var = SELECT-UNASSIGNED-VARIABLE(csp)
     for each value in ORDER-DOMAIN-VALUES(var,assignment,csp) do
         if value is consistent with assignment then
-            add {var = value} to assignment inferences ←INFERENCE(csp,var,value) if inferences ̸= failure then
+            add {var = value} to assignment inferences = INFERENCE(csp,var,value) if inferences == failure then
             add inferences to assignment
-result ← BACKTRACK(assignment, csp) if result ̸= failure then
+result = BACKTRACK(assignment, csp) if result == failure then
 return result
 remove {var = value} and inferences from assignment
 return failure
@@ -127,6 +127,8 @@ class CSP:
         iterations of the loop.
         """
         # TODO: IMPLEMENT THIS
+
+        
         pass
 
     def select_unassigned_variable(self, assignment):
